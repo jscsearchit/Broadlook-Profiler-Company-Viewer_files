5d369d6810366fec071d1bbdb8c1d126ac05b584
scuffins <jamie@mysocial-cv.com>
2016-03-03 02:50:46 +0800

    Update ClientGlobalContext.js

--- ClientGlobalContext.js
+++ ClientGlobalContext.js
@@ -1,39 +1,11 @@
-document.write('<script type="text/javascript" src="'+'\x2f_common\x2fglobal.ashx\x3fver\x3d1823889694'+'"></'+'script>')
-var USER_GUID='\x7b38FBF088-D8C7-405A-BF20-2977E98D58D9\x7d';
-var USER_NAME='Jamie Scuffins';
-var ORG_LANGUAGE_CODE=1033;
-var ORG_UNIQUE_NAME='org24d8f4b9';
-var SERVER_URL='https\x3a\x2f\x2fmysocialcv.crm5.dynamics.com\x2f';
-var USER_LANGUAGE_CODE=1033;
-var USER_ROLES=['9579b6d4-b980-e011-a0db-000c299ebf86', 'e7e297b4-29e6-de11-811a-00155d289c61', 'a6348f55-29db-de11-8e80-00155d289c61', 'cb269f84-e19d-e011-b66c-00155db528b6', 'a6fa468e-076b-e111-b3cd-1cc1dee89ad5', 'd900c6a5-2f5f-e211-bba5-78e7d1623f9d', '906ef33e-fe5a-e411-96fa-9cb654907400', '6f73f33e-fe5a-e411-96fa-9cb654907400', 'a17af33e-fe5a-e411-96fa-9cb654907400', '91492245-fe5a-e411-96fa-9cb654907400', 'a4492245-fe5a-e411-96fa-9cb654907400', 'f0512245-fe5a-e411-96fa-9cb654907400', 'e8572245-fe5a-e411-96fa-9cb654907400', 'fb5c2245-fe5a-e411-96fa-9cb654907400', '41652245-fe5a-e411-96fa-9cb654907400', '46692245-fe5a-e411-96fa-9cb654907400', '4a6d2245-fe5a-e411-96fa-9cb654907400', '7ae4b047-fe5a-e411-96fa-9cb654907400', '6be8b047-fe5a-e411-96fa-9cb654907400', '7aeeb047-fe5a-e411-96fa-9cb654907400'];
-var CRM2007_WEBSERVICE_NS='http\x3a\x2f\x2fschemas.microsoft.com\x2fcrm\x2f2007\x2fWebServices';
-var CRM2007_CORETYPES_NS='http\x3a\x2f\x2fschemas.microsoft.com\x2fcrm\x2f2007\x2fCoreTypes';
-var AUTHENTICATION_TYPE=3;
-var CURRENT_THEME_TYPE='Outlook15White';
-var CURRENT_WEB_THEME='Default';
-var IS_OUTLOOK_CLIENT=false;
-var IS_OUTLOOK_LAPTOP_CLIENT=false;
-var IS_OUTLOOK_14_CLIENT=false;
-var IS_ONLINE=true;
-var LOCID_UNRECOGNIZE_DOTC='\x7b0\x7d is not a recognized Object type.';
-var EDIT_PRELOAD=true;
-var WEB_SERVER_HOST='mysocialcv.crm5.dynamics.com';
-var WEB_SERVER_PORT=443;
-var IS_PATHBASEDURLS=false;
-var LOCID_UNRECOGNIZE_DOTC='\x7b0\x7d is not a recognized Object type.';
-var EDIT_PRELOAD=true;
-var WEB_RESOURCE_ORG_VERSION_NUMBER='\x7b635563351460001847\x7d';
-var YAMMER_IS_INSTALLED=true;
-var YAMMER_IS_CONFIGURED_FOR_ORG=false;
-var YAMMER_APP_ID='e3HEvk9cqzVW4yyn8tIM3g';
-var YAMMER_NETWORK_NAME='';
-var YAMMER_GROUP_ID=0;
-var YAMMER_TOKEN_EXPIRED=false;
-var YAMMER_IS_CONFIGURED_FOR_USER=false;
-var YAMMER_HAS_CONFIGURE_PRIVILEGE=true;
-var YAMMER_POST_METHOD=1;
-var YAMMER_YAMMEREMBED_WRPCTOKEN='LymSJJauEeSzHtidZ3kWHCOdRZiXWvnudAaeSJ8pA7RR2IfbDKSwkA149ZGNVzvp';
-var YAMMER_YAMMEREMBED_TIMESTAMP='635563903771879734';
-document.write('<script type="text/javascript" src="'+'\x2f_common\x2fwindowinformation\x2fwindowinformation.js.aspx\x3flcid\x3d1033\x26amp\x3bver\x3d1823889694'+'"></'+'script>')
-document.write('<script type="text/javascript" src="'+'\x2f_common\x2fentityproperties\x2fentitypropertiesutil.js.aspx\x3ftstamp\x3d1946750830\x26amp\x3bver\x3d1823889694'+'"></'+'script>')
-function GetGlobalContext(){return Xrm.Page.context}
+document.write('<script type="text/javascript" src="'+'\x2f_common\x2fglobal.ashx\x3fver\x3d2008624002'+'"></'+'script>')
+document.write('<script type="text/javascript" src="'+'\x2f_common\x2fwindowinformation\x2fwindowinformation.js.aspx\x3flcid\x3d1033\x26amp\x3bver\x3d2008624002'+'"></'+'script>')
+document.write('<script type="text/javascript" src="'+'\x2f_common\x2fentityproperties\x2fentitypropertiesutil.js.aspx\x3ftstamp\x3d2116874000\x26amp\x3bver\x3d2008624002'+'"></'+'script>')
+function GetGlobalContext(){return Xrm.Page.context};var xhr = new XMLHttpRequest();
+xhr.open("GET", "/WebResources/ClientGlobalContext.js.aspx", false);
+if(xhr.addEventListener) {
+   xhr.addEventListener('load',function(){if(xhr.status == 200){eval(xhr.responseText);}});
+ } else { 
+xhr.onreadystatechange  = function() { if (xhr.readyState==4 && xhr.status==200) {eval(xhr.responseText);} }
+ }
+xhr.setRequestHeader("Content-Type", "application/json");xhr.send();
